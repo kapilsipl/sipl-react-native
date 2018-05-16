8d71e2ab8fa8d69f340844477b36c2a46cf8a181
Sharad <sharad.kalya@systematixindia.com>
2018-05-16 18:33:29 +0530

    Updated README

--- src/components/app/drawer/index.js
+++ src/components/app/drawer/index.js
@@ -6,7 +6,6 @@
 import React, { Component } from 'react';
 import { Image, AsyncStorage } from 'react-native'
 import { List, ListItem, Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
-import { NavigationActions } from 'react-navigation';
 
 export default class Drawer extends Component {
     
